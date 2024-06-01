a38e39ebe5a900086f902b5f08d95b4300c87163
OkampsUni <okamps@uni-muenster.de>
2024-06-01 12:04:13 +0200

    Update urls.py

--- Infodash/infocampboard/urls.py
+++ Infodash/infocampboard/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name="login"),
+    path('', views.loginPage, name="login"),
     path('logout/', views.logoutPage, name="logout"),
     
-    path('', views.home, name="Home"),
+    path('home/', views.home, name="Home"),
     
     path('realtime', views.realTime, name="Realtime"),
     path('contentanalysis', views.contentAnalysis, name="Contentanalysis"),
