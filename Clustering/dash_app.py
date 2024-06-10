28b76a6f14d8d91dbce5cf2d89f2ff3795ca5799
bastwu <bwulf@uni-muenster.de>
2024-06-10 19:04:38 +0200

    Implemented error handling for generating micro-clustering plot to avoid empty plots

--- Clustering/dash_app.py
+++ Clustering/dash_app.py
@@ -9,6 +9,11 @@ from micro_clustering import get_cluster_tweet_data, main_loop
 from Database import Database
 import time
 
+# Setting global variable for last successful generated micro-clustering figure
+last_figure = {'data': [], 'layout': go.Layout(title='Number of Tweets per Cluster Over Time',
+                                               xaxis=dict(title='Time'),
+                                               yaxis=dict(title='Number of Tweets'))}
+
 
 def initialize_dash_app():
     app = dash.Dash(__name__)
@@ -17,7 +22,7 @@ def initialize_dash_app():
         dcc.Graph(id='live-update-graph'),
         dcc.Interval(
             id='interval-component',
-            interval=1*1000,  # in milliseconds (1 second)
+            interval=1 * 1000,  # in milliseconds (1 second)
             n_intervals=0
         )
     ])
@@ -25,59 +30,46 @@ def initialize_dash_app():
     @app.callback(Output('live-update-graph', 'figure'),
                   [Input('interval-component', 'n_intervals')])
     def update_graph_live(n):
-        cluster_tweet_data = get_cluster_tweet_data(db, 'cluster_tweet_data')
+        global last_figure
 
-        # Extracting the '_source' part of each dictionary to create a DataFrame
-        data = [item['_source'] for item in cluster_tweet_data]
-        df = pd.DataFrame(data)
-
-        while df.empty:
-            time.sleep(1)
+        try:
+            # Trying to get cluster data from db
             cluster_tweet_data = get_cluster_tweet_data(db, 'cluster_tweet_data')
+            # Ensure 'timestamp' is in datetime format
+            cluster_tweet_data['timestamp'] = pd.to_datetime(cluster_tweet_data['timestamp'])
 
-            # Extracting the '_source' part of each dictionary to create a DataFrame
-            data = [item['_source'] for item in cluster_tweet_data]
-            df = pd.DataFrame(data)
-
-        # Ensure 'timestamp' is in datetime format
-        try:
-            df['timestamp'] = pd.to_datetime(df['timestamp'])
-        except Exception as e:
-            print(f"Error while accessing timestamp: {e}")
-
-        # Plotting
-        traces = []
-        try:
-            for cluster_id in df['cluster_id'].unique():
-                cluster_data = df[df['cluster_id'] == cluster_id]
+            # Plotting
+            traces = []
+            for cluster_id in cluster_tweet_data['cluster_id'].unique():
+                cluster_data = cluster_tweet_data[cluster_tweet_data['cluster_id'] == cluster_id]
                 traces.append(go.Scatter(
                     x=cluster_data['timestamp'],
                     y=cluster_data['tweet_count'],
                     mode='lines+markers',
                     name=f'Cluster {cluster_id}'
                 ))
+
+            layout = go.Layout(
+                title='Number of Tweets per Cluster Over Time',
+                xaxis=dict(title='Time'),
+                yaxis=dict(title='Number of Tweets')
+            )
+
+            # Update last_figure only if there were no issues while fetching data
+            last_figure = {'data': traces, 'layout': layout}
+
         except Exception as e:
-            print(f"Error while accessing cluster_id: {e}")
-            print(df)
+            print(f"An error occurred: {e}")
 
-        layout = go.Layout(
-            title='Number of Tweets per Cluster Over Time',
-            xaxis=dict(title='Time'),
-            yaxis=dict(title='Number of Tweets')
-        )
-
-        return {'data': traces, 'layout': layout}
+        return last_figure
 
     return app
 
-'''def background_process():
-    main_loop()'''
 
 if __name__ == '__main__':
-
     # Adjust to your own user and id_rsa
-    ssh_user = 'bwulf'
-    ssh_private_key = '/Users/bastianwulf/.ssh/id_rsa_uni'
+    ssh_user = 'USER'
+    ssh_private_key = '/Path/to/.ssh/key'
     tunnel1, tunnel2 = Database.create_ssh_tunnel(ssh_user, ssh_private_key)
     tunnel1.start()
     tunnel2.start()
