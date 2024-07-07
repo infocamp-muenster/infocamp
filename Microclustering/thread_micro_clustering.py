5d61a84bf526d2101282f9e6a5a0f8ae4dc3a579
RomanBoen <150271720+RomanBoen@users.noreply.github.com>
2024-07-07 14:39:37 +0200

    Update thread_micro_clustering.py

--- Microclustering/thread_micro_clustering.py
+++ Microclustering/thread_micro_clustering.py
@@ -4,8 +4,8 @@ from Microclustering.micro_clustering import main_loop
  
 class Micro_Clustering_Thread(threading.Thread):
     def run(self):
-        ssh_user = 'okamps' # 'bwulf'
-        ssh_private_key = '/home/ssh-keys/database_connection' # '/Users/bastianwulf/.ssh/id_rsa_uni'
+        ssh_user = 'r_boen03' # 'bwulf'
+        ssh_private_key = '/Benutzer/Alice/.ssh/id_rsa' # '/Users/bastianwulf/.ssh/id_rsa_uni'
         tunnel1, tunnel2 = Database.create_ssh_tunnel(ssh_user, ssh_private_key)
         tunnel1.start()
         tunnel2.start()
