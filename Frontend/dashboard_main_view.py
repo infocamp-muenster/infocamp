57f752f64a7eff8ad6ea9c5027d538e7b718f9c7
jan-thier <jan.thier@pacemaker.ai>
2024-07-16 13:51:03 +0200

    fix convert date method to use seconds

--- Frontend/dashboard_main_view.py
+++ Frontend/dashboard_main_view.py
@@ -118,10 +118,10 @@ def initialize_dash_app():
 
 def convert_date(date_str):
     # Parse the input date string to a datetime object
-    dt = datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+    dt = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
     # Format the datetime object to the desired output format
-    european_format_date_str = dt.strftime('%d.%m.%Y %H:%M')
+    european_format_date_str = dt.strftime('%d.%m.%Y %H:%M:%S')
 
     return european_format_date_str
 
